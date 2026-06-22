120f64c93bd3e0629d98b6fdf4c49ad68fa92d2c
Mohsan-dev <muhammadmohsan604@gmail.com>
2026-06-22 20:52:46 +0500

    feat: add dynamic destination component with props

--- tour-travel-app/src/components/Navbar.jsx
+++ tour-travel-app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-    const [clicked , setclicked] = useState(false)
+    const [clicked, setclicked] = useState(false)
     const handleclick = () => {
         setclicked(!clicked)
     }
@@ -15,15 +15,16 @@ function Navbar() {
             </h1>
             <div className='menu-icons' onClick={handleclick}>
                 <i className={clicked ? "fa-solid fa-xmark" : "fa-solid fa-bars"}></i>
-              
+
             </div>
 
-            <ul className={clicked ? "nav-menu active" :"nav-menu "}>
+            <ul className={clicked ? "nav-menu active" : "nav-menu "}>
                 {MenuItems.map((item, index) => {
                     return (
                         <li key={index}>
-                            <Link to={item.url} className={item.cName}>{item.icon && <i className={item.icon}></i>}
-                            {item.title}
+                            <Link to={item.url} className={item.cName} onClick={() => setclicked(false)} >{item.icon && <i className={item.icon}></i>}
+                                {item.title}
+
                             </Link>
                         </li>)
                 })}
--- tour-travel-app/src/routes/Home.jsx
+++ tour-travel-app/src/routes/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Hero from '../components/Hero.jsx'
 import homeImg from '../assets/homeImg.jpg'
+import Destination from '../components/Destination.jsx';
 function Home() {
     return ( 
         <>
@@ -12,6 +13,7 @@ function Home() {
           buttontext = "Travel Plan"
           url = "/"
           btnClass = "show" />
+          <Destination/>
         </>
      );
 }
